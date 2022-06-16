b75ddb6f01a58227db4bdd9249556e91bb0f6a01
d3vkk <donaldkk22@yahoo.com>
2022-06-16 15:29:44 +0300

    init

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="text-4xl font-bold">Vue3 Typescript Tailwind Starter</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style scoped></style>
