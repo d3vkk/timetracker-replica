06d727b0fea85fabedee6d0fc9ac79510406f528
d3vkk <donaldkk22@yahoo.com>
2022-06-20 14:57:13 +0300

    feat: Login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import LandingPage from "../views/LandingPage.vue";
 
-const routes: Array<RouteRecordRaw> = [{ path: "/", component: LandingPage }];
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    component: LandingPage,
+  },
+  {
+    path: "/login",
+    component: () => import("../views/Login.vue"),
+  },
+];
 const router = createRouter({
   history: createWebHistory(),
   routes,
