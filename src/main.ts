50ddf590b454e013880d5b446574017b850a9ede
d3vkk <donaldkk22@yahoo.com>
2023-03-01 09:50:24 +0300

    chore: Remove unused state management

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from "vue";
 import router from "./router/index";
-import stores from "./stores/index";
 import App from "./App.vue";
 import "./assets/css/index.css";
 
 const app = createApp(App);
 app.use(router);
-app.use(stores);
 app.mount("#app");
